87025f191fa6498c6af5cb4f9fe48dc731b06303
Yako <gooodniko@gmail.com>
2018-01-31 13:34:06 +0200

    fix code

--- php/send-email.php
+++ php/send-email.php
@@ -1,11 +1,11 @@
 <?php
 
-if($_POST && isset($_FILES['file']))
+if($_POST)
 {
-    $from_email         = 'info@obtpgroup.com'; //from mail, it is mandatory with some hosts
-    $reply_to_email = 'info@obtpgroup.com'; //sender email used in "reply-to" header
+    $from_email         = 'information@obtpgroup.com'; //from mail, it is mandatory with some hosts
+    $reply_to_email = 'information@obtpgroup.com'; //sender email used in "reply-to" header
 
-    $recipient_email    = 'gooodniko@your_domain.com'; //recipient email (most cases it is your personal email)
+    $recipient_email    = 'gooodniko@gmail.com'; //recipient email (most cases it is your personal email)
     
     //Capture POST data from HTML form and Sanitize them, 
     $sender_name    = filter_var($_POST["name"], FILTER_SANITIZE_STRING); //sender name
@@ -32,16 +32,6 @@ if($_POST && isset($_FILES['file']))
     $file_type        = $_FILES['file']['type'];
     $file_error       = $_FILES['file']['error'];
 
-    if($file_error > 0)
-    {
-        die('Upload error or No files uploaded');
-    }
-
-    //read from the uploaded file & base64_encode content for the mail
-    $handle = fopen($file_tmp_name, "r");
-    $content = fread($handle, $file_size);
-    fclose($handle);
-    $encoded_content = chunk_split(base64_encode($content));
 
     $boundary = md5("yakodev");
 
@@ -56,22 +46,43 @@ if($_POST && isset($_FILES['file']))
     $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
     $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
     $body .= chunk_split(base64_encode($message)); 
-    
-    //attachment
-    $body .= "--$boundary\r\n";
-    $body .="Content-Type: $file_type; name=".$file_name."\r\n";
-    $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
-    $body .="Content-Transfer-Encoding: base64\r\n";
-    $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
-    $body .= $encoded_content; 
-    
+
+    if($file_error === UPLOAD_ERR_OK)
+    {
+        //read from the uploaded file & base64_encode content for the mail
+        $handle = fopen($file_tmp_name, "r");
+        $content = fread($handle, $file_size);
+        fclose($handle);
+        $encoded_content = chunk_split(base64_encode($content));
+
+        //attachment
+        $body .= "--$boundary\r\n";
+        $body .="Content-Type: $file_type; name=".$file_name."\r\n";
+        $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
+        $body .="Content-Transfer-Encoding: base64\r\n";
+        $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
+        $body .= $encoded_content; 
+    }
+
     $sentMail = @mail($recipient_email, $subject, $body, $headers);
+
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
     if($sentMail) //output success or failure messages
-    {       
-        die('Thank you for your email');
+    {    
+        if ($lang == "ru"){
+            die('Запрос успешно отправлен');   
+        }else{
+            die('Mail successfully sent');   
+        }
+        // header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
     else
     {
-        die('Could not send mail! Please check your PHP mail configuration.');  
+        if ($lang == "ru"){
+            die('Запрос не был отправлен');   
+        }else{
+            die('Could not send mail');   
+        }
     }
 }
