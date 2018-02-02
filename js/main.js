e2f6bb7daa3d849bf47aa5c54cf712832c08a257
Yako <gooodniko@gmail.com>
2018-02-02 11:53:34 +0200

    fix code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // $(function () {
 //     //Read the cookie, if it has been previously set
 //     var language = $.cookie('language');
@@ -27,52 +26,18 @@
 //                 window.location.href = "../en/index.html";
 //             } 
 //             }
-        
+
 //     }
 // );
 
 
-$(document).ready(function () {
-    $(".menu-link").on("click", function () {
+$(document).ready(function() {
+    $(".menu-link").on("click", function() {
         $("html, body").stop().animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800),
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 800),
             $(".nav").removeClass("act"),
             $(".menuBtn").toggleClass("act"),
             event.preventDefault()
-    }),
-
-        //$("#phone").inputmask("+9(99)99-99-999");
-        $("#nosend-mail").click(function () {
-
-            var name = $("#name").val();
-            var surname = $("#surname").val();
-            var company = $("#company").val();
-            var theme = $("theme").val();
-            var text = $("#text").val();
-            var file = $("#file").val();
-            var phone = $("#phone").val();
-            var mail = $("#mail").val();
-
-            if (mail == "" || phone == "") {
-                $('body').click(false);
-                swal({
-                    title: "Ошибка",
-                    text: "Пожалуйста, заполните необходимые поля",
-                    icon: "error",
-                    button: false,
-                    timer: 1500
-                });
-                return;
-            }
-
-
-            swal({
-                title: "Отправлено",
-                icon: "success",
-                button: "OK",
-            });
-        });
-
-
+    })
 });
